b8997c5db8d94c3db8b55a319c98ce99863fb9c3
codesquadz1606-git <codesquadz1606@gmail.com>
2026-07-22 12:25:50 +0530

    Class 2

---
+++ Class 2/second.py
@@ -0,0 +1,61 @@
+# name="Ayush"
+# city="Ghaziabad"
+# company="Codesquadz"
+
+# print(name)
+# print(f"My name is {name} , I stay in {city} works at {company}")
+
+# a=10; # all the non-decimal numbers are integer.
+# b=34.56 # all the decimal numbers are float.
+# c="AYush" # Any value inside "" is string
+# d=None
+# e=True # Boolean means true or false
+
+# print(type(a) , type(b) , type(c) , type(d) , type(e))
+
+# print(type(a),end=" ")
+# print(type(b),end=" ")
+# print(type(c),end=" ")
+# print(type(d),end=" ")
+# print(type(e),end=" ")
+
+# fname=input("Enter your First Name : ")
+# lname=input("Enter your Last Name : ")
+
+# print(fname,lname);
+
+# a=int(input("Enter a : ")) # string ---> integer
+# b=int(input("Enter b : ")) # string ---> integer
+
+# print(type(a))
+# print(type(b))
+# print(a+b)
+
+# int + int : int
+# int + float : float
+# float + float : float
+# str + str : str
+
+# int + str : error
+# float + str : error
+
+# a=int(input("Enter a : "))
+# b=int(input("Enter b : "))
+
+# print("Sum of a & b is :",a+b)
+# print(f"Sum of {a} & {b} is {a+b}")
+
+# Q1 . Take an input of name , age & city and print them in a statement.
+
+# name=input("Enter name : ")
+# age=input("Enter age : ")
+# city=input("Enter city : ")
+
+# print(f"My name is {name} , my age is {age} & I live in {city}");
+
+# Q2 . Calculate the area of rectangle taking sides as in user input
+
+length=int(input("Enter the length : "));
+width=int(input("Enter the width : "))
+
+print(f"Area of rectangle is {length*width}")
