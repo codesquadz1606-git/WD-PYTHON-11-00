a40f3c0a20e939feae25a314b54010254d5736c7
codesquadz1606-git <codesquadz1606@gmail.com>
2026-07-22 12:25:41 +0530

    Class 1

---
+++ Class 1/first.py
@@ -0,0 +1,22 @@
+# print("Ayush Kumar")
+# print("This is First CLass of Python")
+
+# print("How are you?") # THis is for statement
+
+# print(10-10) # this is for operation .
+
+# # Comment : SIngle Line
+
+# '''
+#     for multiple line comment
+# '''
+
+# print("My name is Ayush. I scored", 40,"in maths" , 60 ,"in science and my total is",40+60)
+
+# print(f"My name is Ayush I scored {40} in maths {60} in science and my total is {40+60}")
+
+# print("Hello \nMy name is Uday.") # next line
+# print("Hello \tMy name is Uday.") # tab space.
+
+print("Hello",end=" ")
+print("My name is Mansi")
